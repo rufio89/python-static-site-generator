f0f58708024b068ccfedcd3c74a314ad2a762bf9
Ryan Krienitz <r.krienitz@gmail.com>
2022-01-08 23:14:18 -0500

    A

--- ssg/site.py
+++ ssg/site.py
@@ -33,7 +33,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            self.error("No parsers for the {} extension, file skipped!".format(path.suffix))
+            self.error(
+                "No parser for the {} extension, file skipped!".format(path.suffix)
+            )
 
 
     @staticmethod
